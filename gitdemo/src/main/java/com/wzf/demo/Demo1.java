eabe45a2bd81ddfb68f7a1e982f177acea3845a4
wuzefang <iwuzefang@163.com>
2021-03-09 13:28:01 +0800

    第一次提交

---
+++ gitdemo/src/main/java/com/wzf/demo/Demo1.java
@@ -0,0 +1,11 @@
+package com.wzf.demo;
+
+/**
+ * @author zefangwu
+ * @create 2021-03-09 13:26
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("第一次提交！");
+    }
+}
