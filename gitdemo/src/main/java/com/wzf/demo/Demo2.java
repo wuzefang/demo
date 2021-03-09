533b60b36198714064da591a79822adae2f7e6eb
wuzefang <iwuzefang@163.com>
2021-03-09 13:33:09 +0800

    wzf分支的第二个类

---
+++ gitdemo/src/main/java/com/wzf/demo/Demo2.java
@@ -0,0 +1,11 @@
+package com.wzf.demo;
+
+/**
+ * @author zefangwu
+ * @create 2021-03-09 13:30
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("wzf分支的第二个类");
+    }
+}
