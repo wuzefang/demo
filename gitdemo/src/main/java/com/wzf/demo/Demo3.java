3ec908a0e3c684e40b33c68069cfe3dc83dcea96
wuzefang <iwuzefang@163.com>
2021-03-09 13:41:02 +0800

    new_wzf03的第三个类

---
+++ gitdemo/src/main/java/com/wzf/demo/Demo3.java
@@ -0,0 +1,11 @@
+package com.wzf.demo;
+
+/**
+ * @author zefangwu
+ * @create 2021-03-09 13:40
+ */
+public class Demo3 {
+    public static void main(String[] args) {
+        System.out.println("new_wzf03创建的第三个类");
+    }
+}
