a275001b0e4ae10aaa223f90691b8ce140fbac8b
wuzefang <iwuzefang@163.com>
2021-03-09 15:11:52 +0800

    Demo6_master

---
+++ gitdemo/src/main/java/com/wzf/demo/Demo6_master.java
@@ -0,0 +1,11 @@
+package com.wzf.demo;
+
+/**
+ * @author zefangwu
+ * @create 2021-03-09 15:11
+ */
+public class Demo6_master {
+    public static void main(String[] args) {
+        System.out.println("Demo6_master....");
+    }
+}
