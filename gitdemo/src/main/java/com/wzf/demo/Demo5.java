e4ebcf3888b9c0c8da3c1b592948c2e73f29f243
wuzefang <iwuzefang@163.com>
2021-03-09 14:39:42 +0800

    master 的 demo5

---
+++ gitdemo/src/main/java/com/wzf/demo/Demo5.java
@@ -0,0 +1,11 @@
+package com.wzf.demo;
+
+/**
+ * @author zefangwu
+ * @create 2021-03-09 14:39
+ */
+public class Demo5 {
+    public static void main(String[] args) {
+        System.out.println("master 的 demo5");
+    }
+}
