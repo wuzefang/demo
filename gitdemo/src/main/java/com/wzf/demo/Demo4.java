d46548e7cb6ce71f749cc49e98e3b9ae0e111a5b
wuzefang <iwuzefang@163.com>
2021-03-09 13:53:27 +0800

    master的第四个类

---
+++ gitdemo/src/main/java/com/wzf/demo/Demo4.java
@@ -0,0 +1,11 @@
+package com.wzf.demo;
+
+/**
+ * @author zefangwu
+ * @create 2021-03-09 13:52
+ */
+public class Demo4 {
+    public static void main(String[] args) {
+        System.out.println("master创建的第四个类");
+    }
+}
