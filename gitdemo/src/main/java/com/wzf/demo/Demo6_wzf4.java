e332a2a97a013ff32a7bf093b9e9b227018584e0
wuzefang <iwuzefang@163.com>
2021-03-09 15:13:46 +0800

    Demo6_wzf4

---
+++ gitdemo/src/main/java/com/wzf/demo/Demo6_wzf4.java
@@ -0,0 +1,11 @@
+package com.wzf.demo;
+
+/**
+ * @author zefangwu
+ * @create 2021-03-09 15:13
+ */
+public class Demo6_wzf4 {
+    public static void main(String[] args) {
+        System.out.println("Demo6_wzf4...");
+    }
+}
